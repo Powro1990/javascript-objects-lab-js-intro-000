4f941c5c9d5146e58996c82d3bb68b0367be12af
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-06 10:54:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, valu){
   return Object.assign(object, { [key]: value });
 }
+
+function deleteFromObjectByKey(object, key){
+  
+}
